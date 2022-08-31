ae5a39e8af510090d3df5b3ed9018db3d10591df
Michael <michael.robertson121@gmail.com>
2022-08-31 03:44:04 -0400

    cleaned and wrote ideas for improving time complexity

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Classic snake game
+
+Inputs are w (up), s (down), a (left), d (right)
--- snake.py
+++ snake.py
@@ -1,16 +1,15 @@
-import tty, sys, termios, time
+import tty, sys, termios, time, select
 from random import randrange
 from itertools import product
-import sys, select
 
-
-# letter_to_direction_dict = {"a":"l", "s":"d", "d":"r", "w":"u"}
-letter_to_opposite_direction_dict = {(-1,0):(1,0),(1,0):(-1,0),(0,1):(0,-1),(0,-1):(0,1)}
 letter_to_change_vector = {'a':(-1,0),'d':(1,0),'s':(0,1),'w':(0,-1)}
 
-board_width = 50
-board_length = 50
+# game settings
+board_width = 16
+board_length = 16
 snake_len = 3
+
+# starting settings
 full_snake_pos = [(5,5)]
 free_positions = set([ele for ele in product(range(1,board_width), range(1,board_length))])
 snake_head = [5,5]
@@ -18,53 +17,37 @@ direction = (1,0)
 food = (randrange(1,board_width-1), randrange(1,board_length-1))
 
 
-
 def read_input(cur_direction, seconds=.2):
     filedescriptors = termios.tcgetattr(sys.stdin)
     tty.setcbreak(sys.stdin)
     
     st = time.time()
-    i, o, e = select.select( [sys.stdin], [], [], seconds)
-    if i:
-        i = sys.stdin.read(1)[0]
-        if i in letter_to_change_vector:
-            direction = letter_to_change_vector[i]
+    input, _, __ = select.select( [sys.stdin], [], [], seconds)
+    if input:
+        input = sys.stdin.read(1)[0]
+        if input in letter_to_change_vector:
+            direction = letter_to_change_vector[input]
+            
             elapsed_time = time.time() - st
-            time.sleep(abs(seconds-(elapsed_time)))
-            if cur_direction != letter_to_opposite_direction_dict[direction]:
+            time.sleep(abs(seconds-(elapsed_time))) # keep movement of snake constant
+            
+            if cur_direction != (direction[0] * -1, direction[1] * -1): # can't double back
                 return direction
-            else:
-                return
-        else:
-            print("Press a,s,d,w and nothing else!")
-            exit(0)
-    else:
-        return
-        
-
-# use elementwise tuple addtion with dictionary to make change
+                
+                
 def update_position(direction, positions, head, snake_len):
     head = [sum(z) for z in zip(head,direction)]
-    # if direction == "l":
-#         head[0] -= 1
-#
-#     if direction == "r":
-#         head[0] += 1
-#
-#     if direction == "d":
-#         head[1] += 1
-#
-#     if direction == "u":
-#         head[1] -= 1
         
     if head[0] == 0 or head[0] >= board_width or head[1] == 0 or head[1] >= board_length:
         print("Out of Bounds!")
         exit(0)
         
     positions.append((head[0],head[1]))
+    
     if (head[0],head[1]) not in free_positions:
-        print("OUROBOROS")
+        print("OUROBOROS") # game over
         exit()
+        
     free_positions.remove((head[0],head[1]))
     
     if len(positions) > snake_len:
@@ -72,8 +55,7 @@ def update_position(direction, positions, head, snake_len):
         positions = positions[1:]
     
     return (head, positions)
-
-
+    
 
 # could change to dictionary from (i,j) entry to correct emoji instead of maintaining lists
 def print_board(full_snake_pos, food):
@@ -97,16 +79,17 @@ def print_board(full_snake_pos, food):
             else:
                 print('  ', end='')
         print()
-
-            
-    
+        
 while True:
     print_board(full_snake_pos, food)
     direction = read_input(direction) or direction
     snake_head, full_snake_pos = update_position(direction, full_snake_pos, snake_head, snake_len)
-    if (snake_head[0], snake_head[1]) == food:
+    if tuple(snake_head) == food:
         snake_len += 1
         food = free_positions.pop()
-        free_positions.add(food)
-
-    
+        free_positions.add(food) # don't die if you hit the food 
+        
+# make a dictionary from pixels to whether they're free or not for checking if you ate yourself
+# then have the inverse of that. Dictionary from free/not free to list of all the free/not free
+# cells, so you can randomly choose from the list. O(2n) memory but only
+# O(n) time.    
